a9cf00ea14dd5aa3bbd27468f16bfbc3fdb5bb8f
Diogo <diogoguerrero.garcia@gmail.com>
2026-09-15 14:40:24 -0500

    fix: Ocultar advertencia de serializacion PageImpl en Spring Boot

--- minimarket-backend/src/main/java/com/minimarket/MinimarketBackendApplication.java
+++ minimarket-backend/src/main/java/com/minimarket/MinimarketBackendApplication.java
@@ -3,10 +3,12 @@ package com.minimarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class MinimarketBackendApplication {
 
     public static void main(String[] args) {
